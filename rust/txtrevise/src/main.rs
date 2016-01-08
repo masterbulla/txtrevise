a427411d97d0c632202f5de9fc3aad73dc0b83e8
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2016-01-08 20:45:39 +0000

    Update main.rs

--- rust/txtrevise/src/main.rs
+++ rust/txtrevise/src/main.rs
@@ -6,25 +6,26 @@
     Released under the MIT License.
 */
 
+extern crate clioptions;
 extern crate regex;
-use std::env;
+use clioptons::CliOptions;
+use regex::Regex;
 use std::io::{BufRead, BufReader, Write};
 use std::fs::File;
-use regex::Regex;
 
 // Display error and then usage information.
-fn display_error(err: &str) {
+fn display_error(program: &str, err: &str) {
     println!("Error: {}.", err);
-    display_usage();
+    display_usage(&program, -1);
 }
 
 /// Display usage information.
-fn display_usage() {
+fn display_usage(program: &str, code: i32) {
     println!("\nTxtrevise v 1.0.0 (rust)");
     println!("Command line text editing tool");
     println!("Copyright (c) 2015 Sam Saint-Pettersen");
     println!("\nReleased under the MIT License");
-    println!("\nUsage: {} [-h] (-q) -f <file> -l <line #> -m <word(s)>", program());
+    println!("\nUsage: {} [-h] (-q) -f <file> -l <line #> -m <word(s)>", program);
     println!("-r <word(s)>");
     println!("\n-f: File to edit");
     println!("-l: Line number to edit text on (starts at 1)");
@@ -32,6 +33,7 @@ fn display_usage() {
     println!("-r: Replacement word(s) for matched word(s)");
     println!("-q: Quiet mode. Only output to console for errors");
     println!("-h: This help information");
+    exit(code);
 }
 
 /// Process file.
@@ -85,79 +87,55 @@ fn match_replace(line: &str, line_no: usize, matches: &str, repl: &str, verbose:
     new_line
 }
 
-/// Get next argument.
-fn next_argument(i: usize) -> String {
-    let a = env::args().nth(i + 1);
-    let arg = match a {
-        Some(a) => a,
-        None => {
-            display_error("Invalid argument");
-            return String::new();
-        }
-    };
-    arg
-}
-
-/// Get program name from first argument.
-fn program() -> String {
-    let a = env::args().nth(0);
-    let arg = match a {
-        Some(a) => a,
-        None => return "txtrevise".to_string(),
-    };
-    arg
-}
-
-/// Entry method.
 fn main() {
-    let args: Vec<_> = env::args().collect();
-    let mut i: usize = 0;
+    let cli = CliOptions::new("txtrevise");
+    let program = cli.get_program();
+    let args = cli.get_args();
     let mut filename = String::new();
     let mut matches = String::new();
     let mut repl = String::new();
     let mut verbose: bool = true;
-    let mut line_no: usize = 1;
-    if args.len() > 1 {
-        for a in args {
+    let mut line_no: usize = 0;
+    if cli.get_num() > 1 {
+        for (i, a) in args.iter().enumerate() {
             if a == "-h" {
                 display_usage();
             }
             if a == "-f" {
-                filename = next_argument(i);
+                filename = cli.next_argument(i);
             }
             if a == "-l" {
-                let l = next_argument(i);
+                let l = cli.next_argument(i);
                 let ol = l.parse::<usize>().ok();
                 line_no = match ol {
                     Some(line_no) => line_no,
                     None => {
-                        display_error("Line number must be an integer");
+                        display_error(&program, "Line number must be an integer");
                         return;
                     }
                 };
                 if line_no == 0 {
-                    display_error("Line number must be greater than 0");
+                    display_error(&program, "Line number must be greater than 0");
                     return;
                 }
             }
             if a == "-m" {
-                matches = next_argument(i);
+                matches = cli.next_argument(i);
             }
             if a == "-r" {
-                repl = next_argument(i);
+                repl = cli.next_argument(i);
             }
             if a == "-q" {
                 verbose = false;
             }
-            i += 1;
         }
     }
     else {
-        display_error("No options specified");
+        display_error(&program, "No options specified");
     } 
 
     // With necessary arguments, process file.
-    if env::args().len() > 2 && filename.len() > 0 {
+    if cli.get_num() > 2 && filename.len() > 0 {
         process_file(&filename, line_no, &matches, &repl, verbose);
     }
 }
